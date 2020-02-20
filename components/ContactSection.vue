ef67f53a25fed562027ec88e8ae1a99107d3632d
o-fukuda <ringleibee@gmail.com>
2020-02-20 11:12:01 +0900

    fix ContactSection.vue

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -3,7 +3,7 @@
     <no-ssr>
       <div id="wrapper" class="m-auto flex justify-center px-2">
         
-        <!-- contact form desc -->
+        <!-- contact form description -->
         <div
           class="contact-form border-primary-500 border-t-4  bg-light rounded-lg w-full p-4 mb-6 lg:py-8 lg:w-2/3"
         >
@@ -13,8 +13,8 @@
             Let's Play Together.
           </p>
           
-          <!-- MOBILE -->
-          <p class="contact-form__title__left sm:hidden pb-1 w-1/2 border-b inline-block border-primary-500  text-center text-dark text-lg lg:text-2xl mb-4 mr-0">
+          <!-- MOBILE Responsive -->
+          <p class="contact-form__title__mobile sm:hidden pb-1 w-1/2 border-b inline-block border-primary-500  text-center text-dark text-lg lg:text-2xl mb-4 mr-0">
             Let's Play Together.
           </p>
         
@@ -38,7 +38,7 @@
             <!-- name section -->
             <div class="flex flex-col lg:flex-row">
               <div class="lg:flex-1 px-1 mb-2">
-                 <p class="label text-center mt-6">お名前<span class="kome">*</span></p>
+                 <p class="label text-center mt-6">お名前<span class="must">*</span></p>
               </div>
             </div>
 
@@ -56,7 +56,7 @@
             <!-- mail section -->
             <div class="flex flex-col lg:flex-row">
               <div class="lg:flex-1 px-1 mb-2">
-                 <p class="label text-center mt-3">メールアドレス<span class="kome">*</span></p>
+                 <p class="label text-center mt-3">メールアドレス<span class="must">*</span></p>
               </div>
             </div>
 
@@ -74,7 +74,7 @@
             <!-- query section -->
             <div class="flex flex-col lg:flex-row">
               <div class="lg:flex-1 px-1 mb-2">
-                 <p class="label text-center mt-3">お問い合わせ内容<span class="kome">*</span></p>
+                 <p class="label text-center mt-3">お問い合わせ内容<span class="must">*</span></p>
               </div>
             </div>
 
@@ -126,84 +126,9 @@ export default createComponent({
 </script>
 
 <style scoped>
-.contact-form__title__center {
-    margin: 0 auto;
-}
 #wrapper {
   animation: 1.5s appear;
 }
-
-
-/* .contact-form {
-  border-top: 5px solid #D80C18; 
-} */
-
-/* .contact-form > p {
-    font-weight: bold;
-} */
-
-/* .contact-form__title {
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     width: 100%;
- } */
-
-/* .contact-form__title__left {
-    width: 50%;
-    border-right: 1.5px solid #D80C18;
-    display: inline-block;
-} */
-
-/* .contact-form__title__right {
-    width: 50%;
-    border-left: 1.5px solid #D80C18;
-    display: inline-block;
-
-} */
-
-/* .contact-form__title__right__span {
-     font-size: 15px;
-} */
-
-/* .contact-form__content {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-} */
-/* 
-.label {
-    width: 100%;
-    display: block;
-    font-weight: bold;
-} */
-/* 
-input {
-    background-color: #F0F0F0;
-    width: 75%;
-    border-radius: 30px;
-    text-align: center;
-} */
-
-/* .textarea {
-    background-color: #F0F0F0;
-    width: 75%;
-    border-radius: 30px;
-    
-}
-
-.input:focus {
-    background-color: white;
-    border-radius: 30px;
-    border: 1px solid #D80C18;
-    box-shadow: 0px 0px 5px 0px #D80C18;
-}
-
-textarea:focus {
-    background-color: white;
-    border: 1px solid #D80C18;
-    box-shadow: 0px 0px 5px 0px #D80C18;
-} */
 
 :focus {
     box-shadow: 0px 0px 5px 0px #D80C18;
@@ -219,20 +144,9 @@ button:hover  {
     @apply bg-white border-primary-500 text-primary-500;
 }
 
-/* input:placeholder-shown {
-  text-align: left;
-}
-
-textarea:placeholder-shown {
-  text-align: left;
-} */
-
-
-.kome {
+.must {
     @apply text-primary-500;
 }
-
-
 
 .contact-form__content {
     @apply flex w-full justify-center;
@@ -258,8 +172,12 @@ ul.list-menu > li:not(:last-child)::after {
 }
 
 *:focus {
-outline: none;
+  @apply outline-none;
 }
+:placeholder-shown {
+    @apply text-left;
+  }
+  
 
 @keyframes appear {
   0% {
