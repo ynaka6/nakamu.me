ce7e39c657ec85a6c9b85bdc498fa47ee4166f5e
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2020-03-14 20:59:50 +0800

    refactor: deleted skill page & changed header layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,6 @@ export default {
   generate: {
     routes: [
       '/about',
-      '/skills',
       '/works',
       '/contents',
       '/contact',
--- src/layouts/DefaultLayout/DefaultHeader.vue
+++ src/layouts/DefaultLayout/DefaultHeader.vue
@@ -2,22 +2,42 @@
   <header class="header">
     <nav class="nav">
       <div class="container">
-        <div class="flex items-center justify-between">
-          <nuxt-link to="/" class="text-primary-500 p-3 hover:opacity-75">
+        <div class="flex flex-col lg:flex-row items-center justify-between">
+          <nuxt-link
+            to="/"
+            class="text-primary-500 text-center p-3 pb-1 lg:pb-3 lg:text-left hover:opacity-75"
+          >
             <h1 class="text-2xl font-leckerli-one">
               <font-awesome-icon :icon="['fas', 'fire']" />
               Nakamu
             </h1>
-            <p class="text-xs">
+            <p class="lg:block text-xs">
               世界を旅して暮らしたい放浪エンジニア
             </p>
           </nuxt-link>
-          <div class="hidden lg:block text-primary-500">
+          <div class="block text-primary-500">
             <ul class="flex font-quicksand">
               <li v-for="(menu, index) in menuList" :key="index">
-                <nuxt-link :to="menu.to" class="menu-link mr-4">
+                <nuxt-link
+                  v-if="menu.to"
+                  :to="menu.to"
+                  class="menu-link"
+                  :class="{ 'mr-2 lg:mr-4': index < menuList.length }"
+                >
                   {{ menu.text }}
                 </nuxt-link>
+                <a
+                  v-else-if="menu.href"
+                  :href="menu.href"
+                  :target="menu.target || `_self`"
+                  class="menu-link mr-4"
+                  :class="{ 'mr-2 lg:mr-4': index < menuList.length }"
+                >
+                  {{ menu.text }}
+                  <sup>
+                    <font-awesome-icon :icon="['fas', 'external-link-alt']" />
+                  </sup>
+                </a>
               </li>
             </ul>
           </div>
@@ -49,20 +69,20 @@ export default defineComponent({
   @apply w-full z-10;
 }
 .nav {
-  @apply w-full items-center justify-between flex-wrap px-6 bg-white;
+  @apply w-full items-center justify-between flex-wrap;
 }
 .container {
   @apply mx-auto;
 }
 .menu-link {
-  @apply relative inline-block no-underline;
+  @apply relative inline-block no-underline font-bold text-gray-700;
 }
 .menu-link::after {
   @apply absolute left-0 w-full;
   bottom: -6px;
   content: '';
   height: 4px;
-  background: #45b7ff;
+  background: theme('colors.red.500');
   transform: scale(0, 1);
   transform-origin: left top;
   transition: transform 0.5s;
